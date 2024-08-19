58f52896b4d632a9f593ef377c3e21be4878813d
lain <lyx@lyxincyberia.org>
2024-08-19 17:08:16 -0500

    created a controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\SignupController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('main');
 });
 
-Route::get("/signup", function () {
-    return view("auth.signup");
-});
+Route::get("/signup", [SignupController::class, "index"]);
