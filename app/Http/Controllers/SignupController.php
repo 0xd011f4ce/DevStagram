a96a5a36c936a28066e8e4a78ca5a5ea58bf5375
lain <lyx@lyxincyberia.org>
2024-08-19 19:31:15 -0500

    now users can be created

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class SignupController extends Controller
 {
@@ -13,12 +17,24 @@ class SignupController extends Controller
 
     public function store(Request $request)
     {
+        // modify request
+        $request->request->add([
+            "username" => Str::slug($request->username),
+        ]);
+
         // Validate the request...
         $request->validate([
             "name" => "required|max:16",
             "username" => "required|unique:users|min:3|max:16",
             "email" => "required|unique:users|email|max:64",
             "password" => "required|min:8|max:64|confirmed",
+        ]);
+
+        User::create([
+            "name" => $request->name,
+            "username" => Str::slug($request->username),
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
         ]);
     }
 }
