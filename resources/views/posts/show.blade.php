c040c755745a417542456cb38a3236eeec08a209
lain <lyx@lyxincyberia.org>
2024-08-21 10:35:55 -0500

    showing posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,4 +5,25 @@
 @endsection
 
 @section('content')
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->image }}" alt="{{ $post->title }}">
+
+            <div class="p-3">
+                <p>0 Likes</p>
+            </div>
+
+            <div>
+                <p class="font-bold"> {{ $post->user->username }} </p>
+                <p class="text-sm text-gray-500">
+                    {{ $post->created_at->diffForHumans() }}
+                </p>
+                <p class="mt-5">
+                    {{ $post->description }}
+                </p>
+            </div>
+        </div>
+
+        <div class="md:w-1/2"></div>
+    </div>
 @endsection
