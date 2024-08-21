7ad5b7daf953e87b7120cf5e52b0b0ec7920868c
lain <lyx@lyxincyberia.org>
2024-08-21 10:01:15 -0500

    added a relationship between User, and Posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,13 @@ class Post extends Model
         "image",
         "user_id"
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->select([
+            "id",
+            "name",
+            "username"
+        ]);
+    }
 }
