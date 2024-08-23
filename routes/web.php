ceebeff3ec47f16b1d847ab92c9f58c36d3bcaca
lain <lyx@lyxincyberia.org>
2024-08-23 16:07:27 -0500

    creating main page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
@@ -12,9 +13,7 @@ use App\Http\Controllers\SignupController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('main');
-});
+Route::get('/', HomeController::class)->name("home");
 
 Route::get("/signup", [SignupController::class, "index"])->name("signup");
 Route::post("/signup", [SignupController::class, "store"]);
