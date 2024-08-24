72145261ae9083db84a2fb6d5b33363bb74cb84f
lain <lyx@lyxincyberia.org>
2024-08-23 19:40:11 -0500

    added a component to list posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,6 @@
 
 @section('content')
 
-    @if ($posts->count())
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="{{ route('posts.show', ['post' => $post, 'user' => $post->user]) }}">
-                        <img src="{{ asset('uploads') . '/' . $post->image }}" alt="{{ $post->title }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    @else
-        <p class="text-center">No posts yet. Be the first one!</p>
-    @endif
+    <x-list-post :posts="$posts" />
 
 @endsection
